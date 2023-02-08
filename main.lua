854d9d73e64a3a30e2499daa6239d9b407a6b311
Mattieu ROQUES <matthieu.roques@epitech.eu>
2023-02-08 17:31:21 +0100

    [ADD] template main.lua

---
+++ main.lua
@@ -0,0 +1,15 @@
+function love.load()
+    -- Load the gamestate library (call once at the beginning)
+end
+
+function love.update(dt)
+    -- Update the gamestate library (call every frame)
+end
+
+function love.draw()
+    -- Draw the gamestate library (call every frame)
+end
+
+function love.keypressed(key)
+    -- Pass the keypress to the gamestate library (call for every keypress)
+end
